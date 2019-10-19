7a86fa4b3f1958b86b52d7b99abee9429292bc78
Melissa Domgaard <melydomgaard@Melissas-MacBook-Pro.local>
2019-10-19 14:18:37 -0600

    Fixed filmDiv, now the content shows for opening-crawl of all movies

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import { films } from '/assets/films.js'
+import { films } from './assets/films.js'
 import { people } from '/assets/people.js'
 
 console.log('I am JavaScript running in the page of the paqe')
@@ -8,7 +8,7 @@ let mainArea = document.querySelector('main')
 console.log(films)
 
 films.forEach(function (film) {
-    let filmDiv = documment.createElement('div')
+    let filmDiv = document.createElement('div')
     let filmTitle = document.createElement('h1')
     let filmCrawl = document.createElement('p')
 
@@ -20,36 +20,38 @@ films.forEach(function (film) {
 
     mainArea.appendChild(filmDiv)
 
+});  
 
-    people.forEach(function (people) {
-        let personDiv = document.createElement('div')
-        let name = document.createElement('h1')
-        let gender = document.createElement('p')
-        let pic = document.createElement('img')
+/*
+people.forEach(function (people) {
+    let personDiv = document.createElement('div')
+    let name = document.createElement('h1')
+    let gender = document.createElement('p')
+    let pic = document.createElement('img')
 
-        let charNum = getCharNumber(person.url)
+    let charNum = getCharNumber(person.url)
 
-        name.textContent = person.name
-        gender.textContent = person.gender
-        pic.src = "https://starwars-visualguide.com/assets/img/characters/${charNum}.jpg"
+    name.textContent = person.name
+    gender.textContent = person.gender
+    pic.src = "https://starwars-visualguide.com/assets/img/characters/${charNum}.jpg"
 
 
-        personDiv.appendChild(name)
-        personDiv.appendChild(gender)
-        personDiv.appendChild(pic)
+    personDiv.appendChild(name)
+    personDiv.appendChild(gender)
+    personDiv.appendChild(pic)
 
-        mainArea.append(personDiv)
+    mainArea.append(personDiv)
 
-    })
+}
 
     function getCharNumber(charURL) {
         let end = charURL.lastInderxOf('/')
         let charID = charURL.substring(end - 2, end)
-        if (charID.lastInderxOf('/') !== -1 ) {
-            return charID.slice(1,2)
+        if (charID.lastInderxOf('/') !== -1) {
+            return charID.slice(1, 2)
         } else {
             return charID
         }
     }
 
-}
+} */
